dbcade950902e4a7cb6212ea63c572a876f9296c
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2025-04-30 11:07:20 -0300

    Adiciona barra de pesquisa de livros e estados

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,15 @@
+import ApacheKafka from '../../imagens/ApacheKafka.svg';
+import LiderancaEmDesign from '../../imagens/Lideranca.svg';
+import NodeJs from '../../imagens/Nodejs.svg';
+import Portugol from '../../imagens/Portugol.svg';
+import ReactNative from '../../imagens/ReactNative.svg';
+import Tuning from '../../imagens/Tuning.svg';
+
+export const Livros = [
+    {nome: 'Liderança em Design', id: 1, src:LiderancaEmDesign, alt:'Liderança em Design'},
+    {nome: 'NodeJs', id: 2, src:NodeJs, alt:'NodeJs'},
+    {nome: 'Portugol', id: 3, src:Portugol, alt:'Portugol'},
+    {nome: 'ReactNative', id: 4, src:ReactNative, alt:'ReactNative'},
+    {nome: 'Tuning SQL', id: 5, src:Tuning, alt:'Tuning SQL'},
+    {nome: 'Apache Kafka', id: 6, src:ApacheKafka, alt:'Apache Kafka'}
+]
